75a2b40e10608511187ca15b4a3cb29fde5f43b4
Jordan <jordanlindgren@live.com>
2021-11-29 07:31:19 -1000

    added connections to database

---
+++ client/src/pages/SavedBooks.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { REMOVE_BOOK } from "../utils/mutations";
+import { GET_ME } from "../utils/queries";
+
+import {
+  Jumbotron,
+  Container,
+  CardColumns,
+  Card,
+  Button,
+} from "react-bootstrap";
+import Auth from "../utils/auth";
+import { removeBookId } from "../utils/localStorage";
+
+const SavedBooks = () => {
+  const { loading, error, data } = useQuery(GET_ME, {
+    fetchPolicy: "network-only",
+  });
+  const userData = data?.me || [];
+  // userData has the savedBooks array
+  console.log(userData);
+
+  const [removeBook] = useMutation(REMOVE_BOOK);
+
+  const handleDeleteBook = async (bookId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      // removeBook is sent to Apollo Server
+      const { data } = await removeBook({
+        variables: { bookId },
+      });
+
+      // upon success, remove book's id from localStorage
+      removeBookId(bookId);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // if data isn't here yet, display loading
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+
+  return (
+    <>
+      <Jumbotron fluid className="text-light bg-dark">
+        <Container>
+          <h1>Viewing saved books!</h1>
+        </Container>
+      </Jumbotron>
+      <Container>
+        <h2>
+          {userData.savedBooks.length
+            ? `Viewing ${userData.savedBooks.length} saved ${
+                userData.savedBooks.length === 1 ? "book" : "books"
+              }:`
+            : "You have no saved books!"}
+        </h2>
+        <CardColumns>
+          {userData.savedBooks.map((book) => {
+            return (
+              <Card key={book.bookId} border="dark">
+                {book.image ? (
+                  <Card.Img
+                    src={book.image}
+                    alt={`The cover for ${book.title}`}
+                    variant="top"
+                  />
+                ) : null}
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <p className="small">Authors: {book.authors}</p>
+                  <Card.Text>{book.description}</Card.Text>
+                  <Button
+                    className="btn-block btn-danger"
+                    onClick={() => handleDeleteBook(book.bookId)}
+                  >
+                    Delete this Book!
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
+    </>
+  );
+};
+
+export default SavedBooks;
---
+++ server/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
+
+const db = require("./config/connection");
+
+// The following code is no longer needed in Grapql setup
+// const routes = require('./routes');
+// app.use(routes);
+
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  // define any context here - authMiddleware encodes and decodes tokens back and forth. It passes the client req.s to the resolvers.js as a 'context' object with a user property.
+  context: authMiddleware,
+});
+
+const app = express();
+// The parameter you provide to applyMiddleware is your middleware's top-level representation of your application. In Express applications, this variable is commonly named app.
+// When you pass your app to applyMiddleware, Apollo Server automatically configures various middleware (including body parsing, the GraphQL Playground frontend, and CORS support), so you don't need to apply them with a mechanism like app.use.
+server.applyMiddleware({ app });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+// (path.join(publicPath, 'index.html')); can do as well
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+db.once("open", () => {
+  app.listen(PORT, () =>
+    console.log(
+      `🌍 GraphQL is now listening on localhost:${PORT}${server.graphqlPath}`
+    )
+  );
+});
